71d6485fa06cd96b6e178e6c90c3db7ebda50dae
SeyonY <71526294+SeyonY@users.noreply.github.com>
2025-01-17 16:30:07 -0500

    added function to read pressure sensor data

---
+++ CM4/Core/Inc/utilities.h
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "cmsis_os.h"
+
+#define XGZP6857D_I2C_ADDR      (0x6D<<1)
+#define REG_PRESSURE_MSB         0x06
+#define REG_PRESSURE_CSB         0x07
+#define REG_PRESSURE_LSB         0x08
+#define REG_CMD                  0x30
+
+#define CONVERSION_CMD           0x0A
+#define CONVERSION_TIME_MS       20
+#define K_FACTOR                 64
+
+HAL_StatusTypeDef readPressureSensor(I2C_HandleTypeDef hi2c, float* pressure);
---
+++ CM4/Core/Src/utilities.c
@@ -0,0 +1,33 @@
+#include "utilities.h"
+
+HAL_StatusTypeDef readPressureSensor(I2C_HandleTypeDef hi2c, float* pressure) {
+	HAL_StatusTypeDef status;
+	uint8_t buf[3] = {0};
+	uint8_t cmd = CONVERSION_CMD;
+	int32_t rawPressure;
+
+	// Send command to command register to prepare for data read
+	status = HAL_I2C_Mem_Write(&hi2c, XGZP6857D_I2C_ADDR, REG_CMD, I2C_MEMADD_SIZE_8BIT, &cmd, 1, 100);
+	if (status != HAL_OK) {
+		return status;
+	}
+
+	osDelay(CONVERSION_TIME_MS);
+
+	// Read in data from pressure registers
+	status = HAL_I2C_Mem_Read(&hi2c, XGZP6857D_I2C_ADDR, REG_PRESSURE_MSB, I2C_MEMADD_SIZE_8BIT, buf, 3, 100);
+	if (status != HAL_OK) {
+		return status;
+	}
+
+	// Combine pressure data from different bytes
+	rawPressure = (buf[0] << 16) | (buf[1] << 8) | buf[2];
+
+	// Check if pressure value is negative
+	if (rawPressure & 0x800000) {
+		rawPressure -= (1 << 24);
+	}
+
+	*pressure = (float)rawPressure/K_FACTOR;
+	return status;
+}
